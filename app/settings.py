2db235782b9d4d17036dc4c1f994a03f586e0b2d
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2020-11-28 13:32:30 +0100

    Debug is set by default for now

--- app/settings.py
+++ app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', "a-not-so-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 ALLOWED_HOSTS = ['*', ]
 
 # Application definition
