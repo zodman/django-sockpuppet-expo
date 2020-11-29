5f9dd7afa6d7d1f0df17088e69e0682f6b9d6a62
Andres Vargas <andres@python3.ninja>
2020-11-29 14:52:58 -0600

    remove contents

--- fabfile.py
+++ fabfile.py
@@ -5,16 +5,16 @@ from patchwork.transfers import rsync
 
 exclude_dirs = [".git", "node_modules", ".cache", ".github", "db.sqlite3"]
 
+
 @task
 def deploy(ctx):
     local("yarn build", echo=True)
     local("python manage.py collectstatic --noinput", echo=True)
-    rsync(ctx, "static/", "apps/django-sockpuppet-expo/static/", exclude=exclude_dirs)
+    rsync(ctx, "static/", "apps/django-sockpuppet-expo/static/",
+          exclude=exclude_dirs)
     with ctx.cd("~/apps/django-sockpuppet-expo"):
         ctx.run('git pull')
         with ctx.prefix("source .env/bin/activate"):
-            ctx.run('python -m pip install -r requirements.in')
-            ctx.run('python3.8 manage.py migrate')
+            ctx.run('python -m pip install --upgrade -r requirements.in')
+            ctx.run('python manage.py migrate')
     ctx.run("sudo supervisorctl restart expo:*")
-
-
